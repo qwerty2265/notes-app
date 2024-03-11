4b9699bae5090eecb37dd4cc089fad4c5be151b1
Maks <maks_mun@bk.ru>
2024-03-12 03:28:05 +0500

    Add font load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,33 @@
-import { Stack } from "expo-router"
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router"
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+    initialRouteName: "home",
+};
 
 const Layout = () => {
-    return <Stack />;
+    const [fontsLoaded] = useFonts({
+        UbuntuRegular: require("../assets/fonts/Ubuntu-Regular.ttf"),
+        UbuntuMedium: require("../assets/fonts/Ubuntu-Medium.ttf"),
+        UbuntuBold: require("../assets/fonts/Ubuntu-Bold.ttf"),
+    });
+
+    const onLayoutRootView = useCallback(() => {
+        if(fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+    return (
+        <Stack initialRouteName="home">
+            <Stack.Screen name="home" />
+        </Stack>
+    )
 }
 
 export default Layout;
