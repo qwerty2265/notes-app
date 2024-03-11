93615b6b67db9a5fd0ef25721ce176676a762573
Maks <maks_mun@bk.ru>
2024-03-12 01:29:26 +0500

    Create constans for colors and fonts

---
+++ constants/theme.js
@@ -0,0 +1,17 @@
+const COLORS = {
+    black: "#11140c",
+    white: "#fbfcfa",
+
+    primary: "#85a957",
+    secondary: "#bad19d",
+    accent: "#a6c77b",
+
+    lightGray: "$edefec",
+    darkGray: "#121310",
+}
+
+const FONT = {
+    regular: "UbuntuRegular",
+    medium: "UbuntuMedium",
+    bold: "UbuntuBold"
+}
